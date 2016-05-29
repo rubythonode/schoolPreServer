7cfa8b40d9e41b91a381c5f97bdefb86ece26a24
Cheol <g6lingp@gmail.com>
2016-05-29 14:28:32 +0900

    test login.html

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,9 @@ var path = require('path');
 router.get('/login', function(req, res, next){
     res.sendFile(path.join(__dirname + '/../public/login.html'));
 })
+router.get('/loginComplete', function(req, res, next){
+    res.sendFile(path.join(__dirname + '/../public/loginComplete.html'));
+})
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');

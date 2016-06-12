506e0ba2312a46bd571fcb65f6c006c397283219
Cheol <g6lingp@gmail.com>
2016-06-12 15:42:23 +0900

    add /:school/:lectureCode -GET & /:school/:lectureCode/eval -POST

--- routes/lecture.js
+++ routes/lecture.js
@@ -11,7 +11,6 @@ router.get('/', function(req, res, next) {
 // 누군가가 새로 가입하면 그 사람의 수업 정보를 가지고 새로 lecture을 만들것인가? -> bot 없어도 됨. 
 // 다만 누군가가 아직 등록하지 않은 수업을 볼려고 하면 문제가 생김.
 
-
 // 수업 읽어 들이기
 router.get('/:school/:lectureCode', function (req, res, next) {
     var data = req.body;
@@ -22,15 +21,26 @@ router.get('/:school/:lectureCode', function (req, res, next) {
         school: req.param.school,
         lectureCode: req.param.school
     }
-    modelUser.read(lecture, function (err, user) {
-        if(err) {
-            console.log(err);
-            return next(err);
-        }
-        res.json(user)
-    })
+    modelLecture.findOne(lectureFindCondition)
+        .exec(function (err, lecture) {
+            if(err){
+                res.err(err)
+            }
+            else{
+                res.json(lecture)
+            }            
+        })    
 });
 
+
+// {
+//     report: Boolean,
+//     reportCnt: Number,
+//     test: Boolean,
+//     testCnt: Number,
+//     easy: Number,
+//     totalEval: Number
+// }
 router.post('/:school/:lectureCode/eval', function (req, res, next){
     var data = req.body;
 
@@ -38,22 +48,69 @@ router.post('/:school/:lectureCode/eval', function (req, res, next){
         school: req.param.school,
         lectureCode: req.param.school
     }
+    modelLecture.findOne(lectureFindCondition)
+        .exec(function (err, lecture) {
+            if(err){
+                res.err(err)
+            }
+            else{
+                var newEval = {
+                    totalEval : data.totalEval,
+                    test: data.test,
+                    testCnt: data.testCnt,
+                    report: data.report,
+                    reportCnt: data.reportCnt,
+                    easy: data.easy
+                }
+                lecture.evaluation.push(newEval)
+                lecture.save()
+                res.json(lecture)
+            }
+            
+        })
 })
 
 
 // 수업애 글쓰기. ex) 과제, 시험, 타임라인
+// 일단 사진 빼고 저장.
+// 타임라인은 5종류가 있다고 생각 -> HomeWork, Report, Test, Print, etc
+// {
+//     category: enum('HomeWork, Report, Test, Print, etc')
+//     title: String,
+//     context: String,
+//     Photo: Blob, // 아마? 이 부분은 나중에 다시
+//     schedule: Date or String
+// }
 router.post('/:school/:lectureCode/timeLine', function (req, res, next) {
     var data = req.body;
 
     console.log(req.body);
-    modelUser.update(data, function (err, user) {
-        if(err) {
-            console.log(err);
-            return next(err);
-        }
-        console.log(user)
-        res.json(user)
-    })
+    
+    var lectureFindCondition = {
+        school: req.param.school,
+        lectureCode: req.param.school
+    }
+    modelLecture.findOne(lectureFindCondition)
+        .exec(function (err, lecture) {
+            if(err){
+                res.err(err)
+            }
+            else{
+                // 사진 저장 그 사진 데이터의 url을 photo에 저장.
+                var newTimeLineItem = {
+                    category: data.category,
+                    title: data.title,
+                    context: data.context,
+                    schedule: data.date,
+                }
+                lecture.timeLine.push(newTimeLineItem)
+                lecture.save()
+                res.json(lecture)
+            }
+            
+        })
+
+    
 });
 
 
